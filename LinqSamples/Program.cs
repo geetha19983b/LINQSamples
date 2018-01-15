2a106b3da616b3d31baaf385da2e6befb61a132d
Geethagajalakshmi_B <Geethagajalakshmi_B@CHNMCT142291L>
2018-01-15 20:42:09 +0530

    comments added

--- LinqSamples/Program.cs
+++ LinqSamples/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,22 +28,78 @@ namespace LinqSamples
                     case 3:
                         SumScoresElimatingLowestthreescores();
                         break;
+                    case 4:
+                        findAlbumDuration();
+                        break;
+                    case 5:
+                        ExpandRange();
+                        break;
+                    case 6:
+                        GetOldest();
+                        break;
                     default:
                         Console.WriteLine("byee");
                         break;
                 }
             }
         }
-        //find strings in list which are not subset
+        //get the olderst person in the list
+        static void GetOldest()
+        {
+            var res = "Jason Puncheon, 26/06/1986; Jos Hooiveld, 22/04/1983; Kelvin Davis, 29/09/1976; Luke Shaw, 12/07/1995; Gaston Ramirez, 02/12/1990; Adam Lallana, 10/05/1988"
+        .Split(';')
+        .Select(n => n.Split(','))
+        .Select(n => new { Name = n[0].Trim(), DateOfBirth = DateTime.ParseExact(n[1].Trim(), "d/M/yyyy", CultureInfo.InvariantCulture) })
+        .OrderByDescending(n => n.DateOfBirth);
+       // .Select(n => new { Name = n.Name, Age = GetAge(n.DateOfBirth) });
+
+            foreach (var i in res)
+            {
+              
+                Console.WriteLine(i.Name + " " + i.DateOfBirth);
+            }
+    
+        }
+        //expand range eg 2,5,7,8,9,10 etc for input as 2,5,7-10
+        static void ExpandRange()
+        {
+            var res = "2,5,7-10,11,17-18"
+                 .Split(',')
+                 .Select(x => x.Split('-'))
+                 .Select(p => new { First = int.Parse(p[0]), Last = int.Parse(p.Last()) })
+                .SelectMany(r => Enumerable.Range(r.First, r.Last - r.First + 1));
+
+            foreach(var i in res)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine();
+
+            var res2 = "6,1-3,2-4"
+                .Split(',')
+                .Select(x => x.Split('-'))
+                .Select(p => new { First = int.Parse(p[0]), Last = int.Parse(p.Last()) })
+                .SelectMany(r => Enumerable.Range(r.First, r.Last - r.First + 1))
+                .OrderBy(r => r)
+                .Distinct();
+            foreach (var i in res2)
+            {
+                Console.WriteLine(i);
+            }
+           
+        }
+        //find strings in list which are not subset for given input should not display "ab" and "ef" cause they are already subset of "abc" and "DEf"
         static void NonSunbsetStrings()
         {
             var list = new List<string> { "abc", "def", "ghi", "ab", "cd", "ef" };
             var res = list.Where(x => !list.Any(y => x != y && y.Contains(x)));
-            foreach(var s in res)
+            foreach (var s in res)
             {
                 Console.WriteLine(s);
             }
         }
+        //use double up extension method to display the contents in sequence twice
         static void DoubleUpEx()
         {
             var numbers = new[] { 1, 5, 3 };
@@ -50,6 +107,7 @@ namespace LinqSamples
             numbers.DoubleUp().ToList().ForEach(x => Console.WriteLine(x));
             str.DoubleUp().ToList().ForEach(x => Console.WriteLine(x));
         }
+        //eliminate lowest thress scores in list and sum the rest
         static void SumScoresElimatingLowestthreescores()
         {
             //var scores = "10,5,0,8,10,1,4,0,10,1";
@@ -63,6 +121,16 @@ namespace LinqSamples
      .Sum();
             Console.WriteLine("sum without lease three scores" + res);
         }
-            
+        //find the album total duration
+        static void findAlbumDuration()
+        {
+            var toaldur = "2:54,3:48,4:51,3:32,6:15,4:08,5:17,3:13,4:16,3:55,4:53,5:35,4:24"
+                 .Split(',')
+                 .Select(t => TimeSpan.Parse("0:" + t))
+                 .Aggregate((t1, t2) => t1 + t2);
+
+            Console.WriteLine("Album total duration is : " + toaldur);
+        }
+
     }
 }
